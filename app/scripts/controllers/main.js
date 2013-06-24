646c6a8628b7fb1c2b949b5cbe1c40eb7c86a512
Ben Miles <ben.miles2@gmail.com>
2013-06-24 15:33:24 +0100

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('molarityApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
