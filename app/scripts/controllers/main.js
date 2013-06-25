7aa07ec8985fab5a6e0806a7705438f58c65de65
Ben Miles <ben.miles2@gmail.com>
2013-06-25 18:51:22 +0100

    equations for mass and molarity functioning with units.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,34 +14,88 @@ angular.module('molarityApp').controller('MainCtrl', function ($scope) {
         }
         else return "no calculation selected :(";
     };
+
+/*
+    $scope.molarityUnitFactor = function() {
+        if ($scope.molarityUnit === "si"){
+            $scope.molarityFactor = 1;
+            //return $scope.molarityFactor;
+        } else if ($scope.molarityUnit === "milli"){
+            $scope.molarityFactor = 0.001;
+        
+        } else if ($scope.molarityUnit === "micro"){
+            $scope.molarityFactor = 0.000001;
+
+        } else if ($scope.molarityUnit === "nano"){
+            $scope.molarityFactor = 0.000000001;
+        
+        } else if ($scope.molarityUnit === "pico"){
+            $scope.molarityFactor = 0.000000000001;
+        }
+    };
+    $scope.massUnitFactor = function() {
+        if ($scope.massUnit === "si"){
+            $scope.massFactor = 1;
+        } else if ($scope.massUnit === "milli"){
+            $scope.massFactor = 0.001;
+        
+        } else if ($scope.massUnit === "micro"){
+            $scope.massFactor = 0.000001;
+
+        } else if ($scope.massUnit === "nano"){
+            $scope.massFactor = 0.000000001;
+        
+        }
+    };
+    $scope.volumeFactor = function() {
+        if ($scope.volumeUnit === "si"){
+            return 1;
+        } else if ($scope.volumeUnit === "milli"){
+            return 0.001;
+        
+        } else if ($scope.volumeUnit === "micro"){
+            return 0.000001;
+
+        } else if ($scope.volumeUnit === "nano"){
+            return 0.000000001;
+        
+        } else if ($scope.volumeUnit === "nano"){
+            return 0.000000000001;
+        }
+    };
+*/
     
     $scope.findMolarity = function() {
-      $scope.unknownMolarity =  (1/($scope.knownMw/$scope.knownMass))/$scope.knownVolume;
-      return $scope.unknownMolarity;
+      var mass = $scope.knownMass;
+      var massUnit = $scope.massUnit;
+      var mw = $scope.knownMw;
+      var volume = $scope.knownVolume;
+      var volumeUnit = $scope.volumeUnit;
+        
+      $scope.unknownMolarity = (1/(mw/(mass*massUnit)))/(volume*volumeUnit);
+      return $scope.unknownMolarity + " g/mol";
       };
     
-    $scope.findMass = function(){
-      $scope.unknownMass = ($scope.knownMw * $scope.knownMolarity/$scope.knownVolume);
-      return $scope.unknownMass;
+    $scope.findMass = function(){      
+      var mw = $scope.knownMw;
+      var molarity = $scope.knownMolarity;
+      var molarityUnit = $scope.molarityUnit;
+      var volume = $scope.knownVolume;
+      var volumeUnit = $scope.volumeUnit;
+      
+      $scope.unknownMass = (mw*(molarity*molarityUnit)*(volume*volumeUnit));
+      return $scope.unknownMass + " g";
       };
 
     $scope.isDisabled = function (test){
-         if (test === $scope.radioValue){
-         return true;
-         }
-    } 
+      if (test === $scope.radioValue){
+        return true;
+        };
+      };
+      
+
   }
 );
-
-function DropdownCtrl($scope) {
-  $scope.items = [
-    "M",
-    "mM",
-    "µM",
-    "nM",
-    "pM"
-  ];
-}
 
 var ButtonsCtrl = function ($scope) {
 
